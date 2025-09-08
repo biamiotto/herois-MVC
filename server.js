aaedcc7a139db123f1098048aa57b939bc86b078
Beatriz Miotto <miotto.beatriz2008@gmail.com>
2025-09-08 14:03:31 -0300

    Preenchimento das pastas

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 import express from "express";
+import dotenv from "dotenv";
+import heroisRoutes from "../atividade-herois/src/routes/heroisRoutes.js";
 
 const serverPort = 3000;
 const app = express();
@@ -6,9 +8,11 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Serviodr Funcionando 🚀");
-})
+  res.send("Serviodr Funcionando 🚀");
+});
+
+app.use("/herois", heroisRoutes);
 
 app.listen(serverPort, () => {
-    console.log(`Serviodr Funcionando em http://localhost:${serverPort}`)
+  console.log(`Serviodr Funcionando em http://localhost:${serverPort}`);
 });
